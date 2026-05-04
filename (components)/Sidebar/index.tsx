e80a6f56c29c6025a0896b57466074ce7834f904
buikhacthang91042 <buikhacthang910.42@gmail.com>
2026-05-04 13:08:10 +0700

    feat: basic header

--- (components)/Sidebar/index.tsx
+++ (components)/Sidebar/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/state";
+import { useGetProjectsQuery } from "@/state/api";
 import {
   AlertCircle,
   AlertOctagon,
@@ -31,6 +32,8 @@ interface props {}
 function SideBar() {
   const [showProject, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
+
+  const { data: projects } = useGetProjectsQuery();
   const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed,
@@ -100,7 +103,15 @@ function SideBar() {
           )}
         </button>
         {/* Project list */}
-
+        {showProject &&
+          projects?.map((project) => (
+            <SideBarLink
+              key={project.id}
+              icon={Briefcase}
+              label={project.name}
+              href={`/projects/${project.id}`}
+            />
+          ))}
         {/* Project link */}
         <button
           onClick={() => setShowPriority((prev) => !prev)}
